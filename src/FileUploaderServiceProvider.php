6a4af93231e43a27bd00683dacd5c159a60a405c
Craig Humphreys <craig_h_411@yahoo.co.uk>
2015-12-16 15:57:45 +0000

    Tidied up service provider

--- src/FileUploaderServiceProvider.php
+++ src/FileUploaderServiceProvider.php
@@ -1,42 +1,40 @@
 <?php
 namespace Humps\LaravelFileUploader;
 
-use Exception;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
-class FileUploaderServiceProvider extends ServiceProvider {
+class FileUploaderServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Bootstrap the application services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->loadTranslationsFrom(__DIR__.'/resources/lang/vendor/laravel-file-uploader', 'laravel-file-uploader');
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadTranslationsFrom(__DIR__ . '/resources/lang/vendor/laravel-file-uploader', 'laravel-file-uploader');
 
-		$this->publishes([
-			__DIR__.'/resources/lang/vendor/laravel-file-uploader' => base_path('resources/lang/vendor/laravel-file-uploader')
-		]);
-	}
+        $this->publishes([
+            __DIR__ . '/resources/lang/vendor/laravel-file-uploader' => base_path('resources/lang/vendor/laravel-file-uploader')
+        ]);
+    }
 
-	/**
-	 * Register the application services.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		App::bind('upload', function ()
-		{
-			return new LaravelFileUploader;
-		});
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        App::bind('upload', function () {
+            return new LaravelFileUploader;
+        });
 
-		App::bind('uploadExceptionHandler', function ($app, $params)
-		{
-			return new LaravelFileUploaderExceptionHandler($params[0]);
-		});
+        App::bind('uploadExceptionHandler', function ($app, $params) {
+            return new LaravelFileUploaderExceptionHandler($params[0]);
+        });
 
-	}
+    }
 }
